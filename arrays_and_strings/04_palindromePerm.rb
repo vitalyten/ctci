129ebb86d4a5eb88a5f277c88b5a26e601476289
VitalyTen <vtenigin@student.42.us.org>
2017-04-11 15:17:04 -0700

    04

--- arrays_and_strings/04_palindromePerm.rb
+++ arrays_and_strings/04_palindromePerm.rb
@@ -1,7 +1,14 @@
 #!/usr/bin/ruby
 
 def palPerm(str)
-
+	arr = Array.new(256, 0)
+	odd = 0
+	str.downcase!
+	str.gsub!(" ", "")
+	str.each_codepoint {|c| arr[c] += 1}
+	arr.each {|x| odd += 1 if x % 2 != 0}
+	return true if str.length % 2 == odd
+	return false
 end
 
 str = gets.strip
