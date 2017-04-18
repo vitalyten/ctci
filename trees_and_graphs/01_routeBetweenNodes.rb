1a5e97a89f9f9238fdb2385895e769719fe10a11
VitalyTen <vtenigin@student.42.us.org>
2017-04-17 22:06:58 -0700

    01

--- trees_and_graphs/01_routeBetweenNodes.rb
+++ trees_and_graphs/01_routeBetweenNodes.rb
@@ -1,11 +1,16 @@
 #!/usr/bin/ruby
 
 class Node
-	attr_accessor :name, :neighbors
+	attr_accessor :name, :adjacent, :visited
 
 	def initialize(name)
 		@name = name
-		@edges = []
+		@adjacent = Array.new
+		@visited = false
+	end
+
+	def addAdjacent(n)
+		@adjacent << n
 	end
 end
 
@@ -32,9 +37,47 @@ class Graph
 	end
 
 	def addEdge(name1, name2)
-		n1 = @nodes.index {|n| n.name == name1}
-		n2 = @nodes.index {|n| n.name == name2}
-		@nodes[n1].edges[n2] = true
-		@nodes[n2].edges[n1] = true
+		n1 = @nodes.find {|n| n.name == name1}
+		n2 = @nodes.find {|n| n.name == name2}
+		raise "node not found" if n1 == nil || n2 == nil
+		n1.addAdjacent(n2)
+		n2.addAdjacent(n1)
 	end
+
+	def isConnected(name1, name2)
+		q = Array.new
+		n1 = @nodes.find {|n| n.name == name1}
+		n2 = @nodes.find {|n| n.name == name2}
+		raise "node not found" if n1 == nil || n2 == nil
+		q.push(n1)
+		while !q.empty?
+			n = q.shift
+			n.visited = true
+			nodes = n.adjacent
+			nodes.each {|x| q.push(x) if x.visited == false}
+		end
+		return n2.visited
+	end
+end
+
+graph = Graph.new
+graph.addNode(0)
+graph.addNode(1)
+graph.addNode(2)
+graph.addNode(3)
+graph.addNode(4)
+graph.addNode(5)
+graph.addNode(6)
+graph.addEdge(0, 1)
+graph.addEdge(0, 5)
+graph.addEdge(0, 4)
+graph.addEdge(1, 2)
+graph.addEdge(1, 3)
+graph.addEdge(1, 4)
+graph.addEdge(2, 3)
+graph.addEdge(4, 3)
+if graph.isConnected(5, 2)
+	puts "connected"
+else
+	puts "not connected"
 end
