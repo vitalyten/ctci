f40539d3492db56a228811229ec2405fcf078f62
VitalyTen <vtenigin@student.42.us.org>
2017-04-23 23:33:03 -0700

    09

--- trees_and_graphs/09_BSTSequences.rb
+++ trees_and_graphs/09_BSTSequences.rb
@@ -83,7 +83,7 @@ end
 
 tree = Tree.new
 arr = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30]
-arr1 = [1,2,3,4,5,6]
+arr1 = [1,2,3,4,5,6,7,8,9]
 tree.arrToBST(arr1)
 tree.printTree
 print tree.allSequences, "\n"
