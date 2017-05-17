d3293cb672c4f1c91e34eb2b5fa4542a3432d362
VitalyTen <vtenigin@student.42.us.org>
2017-05-17 16:34:03 -0700

    sparse search

---
+++ sorting_and_searching/05_sparseSearch.rb
@@ -0,0 +1,29 @@
+#!/usr/bin/ruby
+
+def search(arr, left, right, str)
+	return -1 if (str == "" or str == nil)
+	mid = (left + right) / 2
+	if arr[mid] == ""
+		midl = mid - 1
+		midr = mid + 1
+		while true
+			return -1 if (midl < left and midr > right)
+			if (midl >= left and arr[midl] != "")
+				mid = midl
+				break
+			end
+			if (midr <= right and arr[midr] != "")
+				mid = midr
+				break
+			end
+			midl -= 1
+			midr += 1
+		end
+	end
+	return search(arr, left, mid - 1, str) if (str <=> arr[mid]) < 0
+	return search(arr, mid + 1, right, str) if (str <=> arr[mid]) > 0
+	return mid
+end
+
+arr = ["apple", "", "", "", "ball", "", "", "cat", "", "", "dog", "", ""]
+p search(arr, 0, arr.length - 1, arr[4])
